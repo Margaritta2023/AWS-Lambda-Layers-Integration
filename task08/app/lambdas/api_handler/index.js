42dcdba22cbabc7875998d645017766495122e9c
Margaritta2023 <margarittamkn@gmail.com>
2024-12-10 18:43:51 +0400

    add layers

--- task08/app/lambdas/api_handler/index.js
+++ task08/app/lambdas/api_handler/index.js
@@ -1,8 +1,21 @@
+const axios = require("axios"); 
+
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+  try {
+    const weatherApi = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
+
+    const response = await axios.get(weatherApi);
+      return {
+          statusCode: 200,
+          body: JSON.stringify(response.data)  
+      };
+
+  } catch (error) {
+      console.error('Error fetching weather data:', error);
+
+      return {
+          statusCode: 500,
+          body: JSON.stringify({ message: 'Failed to fetch weather data' })
+      };
+  }
 };
